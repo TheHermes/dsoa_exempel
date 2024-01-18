adb027787cd3b6c2052174d2e6834788c68a4982
heikkihe <heikkihe@arcada.fi>
2024-01-18 11:00:03 +0200

    Code Challenge 18.01.2024

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Calculator calc1 = new Calculator();
+        Random random = new Random();
+
+
+        //System.out.printf("Skriv: ");
+
+        //String userName = scanner.nextLine();
+        //System.out.printf("Your name %s\n", userName);
+
+
+        while(true) {
+            System.out.printf("Random number: %d \n(Press ENTER for another random number, Q to exit the program!)",
+                    random.nextInt(0, 100)
+            );
+            String input = scanner.nextLine();
+            if (input.equals("q")){
+                System.out.println("Goodbye...");
+                System.exit(1);
+                break;
+            }
+        };
+
+
+
+
+
+
+        /*while(true){
+            try {
+                System.out.printf("Skriv in ett tal: ");
+                int num1 = Integer.parseInt(scanner.nextLine());
+
+                System.out.printf("\nSkriv in ett till tal: ");
+                int num2 = Integer.parseInt(scanner.nextLine());
+
+                System.out.printf("%d * %d = %.0f", num1, num2, calc1.multiply(num1, num2));
+                break;
+            } catch(NumberFormatException e) {
+                System.out.println("Du måste skriva in ett heltal!");
+            }
+        }*/
+
+
+
+
+        //System.out.println(random.nextInt(99));
+        //System.out.println(random.nextInt(99, 999));
 
         /*Car car = new Car("Brita");
         System.out.println(car.getName() + " " + car.getType());
@@ -17,10 +70,10 @@ public class Main {
         car.soundWarning();*/
 
         // Code Challenge Lektion 3
-        Calculator calc = new Calculator();
+        /*Calculator calc = new Calculator();
         System.out.printf("2 tals multiplikation: %.2f\n", calc.multiply(1, 2));
         System.out.printf("3 tals multiplikation: %.2f\n", calc.multiply(1, 2, 3));
-        System.out.printf("4 tals multiplikation: %.2f", calc.multiply(1, 2, 3, 4));
+        System.out.printf("4 tals multiplikation: %.2f", calc.multiply(1, 2, 3, 4));*/
 
 
 
--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -5,20 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
-    void multiply1() {
+    void multiply2() {
         Calculator calc1 = new Calculator();
         assertEquals(2, calc1.multiply(1, 2));
     }
 
     @Test
-    void testMultiply() {
+    void multiply3() {
         Calculator calc2 = new Calculator();
         assertEquals(6, calc2.multiply(1, 2, 3));
 
     }
 
     @Test
-    void testMultiply1() {
+    void multiply4() {
         Calculator calc3 = new Calculator();
         assertEquals(24, calc3.multiply(1, 2, 3,4));
 
